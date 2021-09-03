f88311a0c3b6fe5ec39d4f14ce541d480f7b765c
Kelvin Tegelaar <kelvin@limenetworks.nl>
2021-09-03 15:19:42 +0200

    update 0.1.1 added syncro support

--- Public/New-SyncroRMMApplication.ps1
+++ Public/New-SyncroRMMApplication.ps1
@@ -12,46 +12,49 @@ function New-SyncroRMMApplication {
         [ValidateNotNullOrEmpty()][String]$SyncroPolicyID,
 
         [parameter(Position = 3, Mandatory = $true)]
-        [ValidateNotNullOrEmpty()][String]$ApplicationId,
+        [ValidateNotNullOrEmpty()][String]$SyncroInstallerURL,
 
         [parameter(Position = 4, Mandatory = $true)]
-        [ValidateNotNullOrEmpty()][String]$ApplicationSecret,
+        [ValidateNotNullOrEmpty()][String]$ApplicationId,
 
         [parameter(Position = 5, Mandatory = $true)]
-        [ValidateNotNullOrEmpty()][String]$YourTenantID,
+        [ValidateNotNullOrEmpty()][String]$ApplicationSecret,
 
         [parameter(Position = 6, Mandatory = $true)]
-        [ValidateNotNullOrEmpty()][String]$refreshtoken,
+        [ValidateNotNullOrEmpty()][String]$YourTenantID,
 
         [parameter(Position = 7, Mandatory = $true)]
-        [ValidateNotNullOrEmpty()][bool]$AssignToAllDevices,
+        [ValidateNotNullOrEmpty()][String]$refreshtoken,
 
         [parameter(Position = 8, Mandatory = $true)]
+        [ValidateNotNullOrEmpty()][bool]$AssignToAllDevices,
+
+        [parameter(Position = 9, Mandatory = $true)]
         [ValidateNotNullOrEmpty()][string]$PackageName,
         
-        [parameter(Position = 9, Mandatory = $true)]
+        [parameter(Position = 10, Mandatory = $true)]
         [ValidateNotNullOrEmpty()][string]$SyncroTenantVariable
 
     )
     Write-Verbose "Connecting to the Graph API for processing all clients"
     Connect-GraphAPI -ApplicationId $ApplicationId -ApplicationSecret $ApplicationSecret -Tenantid $YourTenantID -RefreshToken $refreshtoken
-    Write-Verbose "Connecting to DRMM to get all tenants using tenant variable $DattoTenantVariable"
-    $Tenants = Get-SyncroRMMTenantInfo -URL $SyncroURL -key $SyncroAPIKey -VariableName $SyncroTenantVariable
+    Write-Verbose "Connecting to DRMM to get all tenants using tenant variable $SyncroTenantVariablee"
+    $Tenants = Get-SyncroTenantInfo -URL $SyncroURL -key $SyncroAPIKey -VariableName $SyncroTenantVariable
     foreach ($Tenant in $Tenants) {
         write-verbose "Starting process for tenant $($tenant.name)"
         try {
             write-verbose "Downloading client for $($tenant.name) / $($tenant.tenantid)"
             #need to actually add the downloading of the agent.
-            #  $null = New-item -ItemType Directory "$ENV:Temp\$($tenant.uid)" -ErrorAction SilentlyContinue
-            # (New-Object System.Net.WebClient).DownloadFile("https://pinotage.centrastage.net/csm/profile/downloadAgent/$($tenant.uid)", "$ENV:Temp\$($tenant.id)\SyncroSetup.exe")
+            $null = New-item -ItemType Directory "$ENV:Temp\$($tenant.id)" -ErrorAction SilentlyContinue
+            (New-Object System.Net.WebClient).DownloadFile("$($SyncroinstallerURL)", "$ENV:Temp\$($tenant.id)\SyncroSetup.exe")
             $params = @{
                 packagename          = $PackageName
                 packageversion       = '1.0'
                 packageinstallcmd    = "SyncroSetup.exe --console --customerid $($tenant.id) --policyid $SyncroPolicyID"
-                packageuninstallcmd  = "C:\Program Files (x86)\CentraStage\uninst.exe /S"
-                packagedetectionpath = "C:\Programdata\CentraStage\AEMAgent"
-                packagedetectionfile = "AEMAgent.exe"
-                InstallerPath        = "$ENV:Temp\$($tenant.id)\SyncroSetup.exe.exe"
+                packageuninstallcmd  = "SyncroSetup.exe /X"
+                packagedetectionpath = "C:\Program Files\RepairTech\Syncro"
+                packagedetectionfile = "Update.exe"
+                InstallerPath        = "$ENV:Temp\$($tenant.id)\SyncroSetup.exe"
                 TenantID             = $Tenant.tenantid
             }
             write-verbose "Starting to create package for $($tenant.name)"
